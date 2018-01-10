a2521b74bda55a74b1fb9b6d58cf1ae1b7c3cc3a
Felipe García <felipe@toptierlabs.com>
2018-01-10 17:10:07 -0300

    Added missing input height styling

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, TextInput, Text } from 'react-native';
+import styles from './styles';
 
 const Input = ({ input: { onChange, ...restInput }, password = false, label, meta: { touched, error } }) => (
   <View>
     {label && <Text>{label}</Text>}
     <View>
-      <TextInput onChangeText={onChange} {...restInput} secureTextEntry={password} />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChange}
+        secureTextEntry={password}
+        {...restInput}
+      />
       {touched && error && <Text>{error}</Text>}
     </View>
   </View>
---
+++ src/components/common/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    height: 50,
+  }
+});
+
+export default styles;
