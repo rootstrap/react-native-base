4b194a31710508baf2fe87a74c474bf6ddc53616
Felipe García <felipe@toptierlabs.com>
2018-01-12 13:43:23 -0300

    React Native Navigator set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,59 @@
-import React, { Component } from 'react';
+import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 import Immutable from 'immutable';
 import { sessionService } from 'redux-react-native-session';
 
-import AppWithNavigationState from '../navigation/AppNavigator';
+import registerScreens from '../screens';
 import configureStore from '../store/configureStore';
 
-class App extends Component {
-  componentWillMount() {
-    sessionService.initSessionService(this.store);
+const store = configureStore(Immutable.Map());
+registerScreens(store, Provider);
+
+class App {
+  constructor() {
+    this.appInitialized = false;
+    this.authenticated = false;
+    sessionService.initSessionService(store);
+
+    store.subscribe(this.onStoreUpdate.bind(this));
   }
 
-  store = configureStore(Immutable.Map());
+  onStoreUpdate() {
+    const session = store.getState().get('session');
+    const authenticated = session.get('authenticated');
+    const user = session.get('user');
+    const shouldUpdate = this.authenticated !== authenticated && (!authenticated || !user.isEmpty());
+    if (!this.appInitialized) {
+      const checked = session.get('userChecked');
+      if (checked) {
+        this.appInitialized = true;
+        this.authenticated = authenticated;
+        this.startApp(authenticated);
+      }
+    } else if (shouldUpdate) {
+      this.authenticated = authenticated;
+      this.startApp(authenticated);
+    }
+  }
 
-  render() {
-    return (
-      <Provider store={this.store}>
-        <AppWithNavigationState />
-      </Provider>
-    );
+  startAuthenticatedApp() {
+    Navigation.startSingleScreenApp({
+      screen: {
+        screen: 'reactnativebase.MainScreen'
+      }
+    });
+  }
+
+  startApp(authenticated) {
+    if (authenticated) {
+      this.startAuthenticatedApp();
+    } else {
+      Navigation.startSingleScreenApp({
+        screen: {
+          screen: 'reactnativebase.LoginScreen'
+        }
+      });
+    }
   }
 }
 
---
+++ src/screens.js
@@ -0,0 +1,11 @@
+import { Navigation } from 'react-native-navigation';
+
+import LoginScreen from './containers/LoginScreen';
+import MainScreen from './containers/MainScreen';
+
+const registerScreens = (store, Provider) => {
+  Navigation.registerComponent('reactnativebase.LoginScreen', () => LoginScreen, store, Provider);
+  Navigation.registerComponent('reactnativebase.MainScreen', () => MainScreen, store, Provider);
+};
+
+export default registerScreens;
