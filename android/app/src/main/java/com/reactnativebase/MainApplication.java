2a004a55871e6c76c6fac94c6a854a9477f46b40
Felipe García <felipe@toptierlabs.com>
2018-01-12 13:43:23 -0300

    React Native Navigator set up

--- android/app/src/main/java/com/reactnativebase/MainApplication.java
+++ android/app/src/main/java/com/reactnativebase/MainApplication.java
@@ -8,11 +8,12 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -28,6 +29,25 @@ public class MainApplication extends Application implements ReactApplication {
       );
     }
   };
+
+  @Override
+  public boolean isDebug() {
+     // Make sure you are using BuildConfig from your own application
+     return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+     // Add additional packages you require here
+     // No need to add RnnPackage and MainReactPackage
+     return Arrays.<ReactPackage>asList(
+         // eg. new VectorIconsPackage()
+     );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+     return getPackages();
+  }
 
   @Override
   public ReactNativeHost getReactNativeHost() {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as form } from 'redux-form/immutable';
-import { sessionReducer as session } from 'redux-react-native-session';
-import nav from './navReducer';
+import { sessionImmutableReducer as session } from 'redux-react-native-session';
 
 const AppReducer = combineReducers({
-  nav,
   form,
   session
 });
