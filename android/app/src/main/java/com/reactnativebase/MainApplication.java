3fb8ebe214abc98a16c28f1767f60350e89cbb14
juanlacruz <juanlacu@gmail.com>
2018-02-22 14:15:06 -0300

    Fixed React Native Config on Android

--- android/app/src/main/java/com/reactnativebase/MainApplication.java
+++ android/app/src/main/java/com/reactnativebase/MainApplication.java
@@ -40,7 +40,8 @@ public class MainApplication extends NavigationApplication {
      // Add additional packages you require here
      // No need to add RnnPackage and MainReactPackage
      return Arrays.<ReactPackage>asList(
-         // eg. new VectorIconsPackage()
+         new MainReactPackage(),
+         new ReactNativeConfigPackage()
      );
   }
 
