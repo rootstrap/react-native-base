4e0fdd515344e2879f003f9b5d0a4a1a8ace0b63
Elizabeth Lofredo <elizabeth@Elizabeths-Mac-mini.local>
2018-04-27 16:43:06 -0300

    upgrade react-native version 0.55.3

--- android/app/src/main/java/com/reactnativebase/MainApplication.java
+++ android/app/src/main/java/com/reactnativebase/MainApplication.java
@@ -1,63 +1,31 @@
 package com.reactnativebase;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+import android.support.annotation.Nullable;
 import com.reactnativenavigation.NavigationApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
+
 public class MainApplication extends NavigationApplication {
+     @Override
+     public boolean isDebug() {
+          return BuildConfig.DEBUG;
+     }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Nullable
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return Arrays.<ReactPackage>asList(
+            new ReactNativeConfigPackage()
+        );
     }
 
+    @Nullable
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new ReactNativeConfigPackage()
-      );
+    public String getJSMainModuleName() {
+      return "index.android";
     }
-  };
-
-  @Override
-  public boolean isDebug() {
-     // Make sure you are using BuildConfig from your own application
-     return BuildConfig.DEBUG;
-  }
-
-  protected List<ReactPackage> getPackages() {
-     // Add additional packages you require here
-     // No need to add RnnPackage and MainReactPackage
-     return Arrays.<ReactPackage>asList(
-         new MainReactPackage(),
-         new ReactNativeConfigPackage()
-     );
-  }
-
-  @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-     return getPackages();
-  }
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
