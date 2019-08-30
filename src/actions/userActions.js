7e72dcbc25d8563e9055dad48b99ee80c492200f
Mauricio Cousillas <cousillas806@gmail.com>
2019-08-30 09:27:17 -0300

    Status reducer PoC (#74)
    Removed submitting prop from login form
    
    Improvements

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -5,23 +5,63 @@ import userService from 'services/userService';
 
 export const actionTypes = {
   LOGIN_SUCCESS: 'LOGIN_SUCCESS',
+  LOGIN_REQUEST: 'LOGIN_REQUEST',
+  LOGIN_ERROR: 'LOGIN_ERROR',
+  SIGN_UP_SUCCESS: 'SIGN_UP_SUCCESS',
+  SIGN_UP_REQUEST: 'SIGN_UP_REQUEST',
+  SIGN_UP_ERROR: 'SIGN_UP_ERROR',
   LOGOUT_SUCCESS: 'LOGOUT_SUCCESS',
+  LOGOUT_REQUEST: 'LOGOUT_REQUEST',
+  LOGOUT_ERROR: 'LOGOUT_ERROR',
 };
 
-export const loginSuccess = () => ({
+const loginSuccess = () => ({
   type: actionTypes.LOGIN_SUCCESS,
 });
 
-export const logoutSuccess = () => ({
+const loginRequest = () => ({
+  type: actionTypes.LOGIN_REQUEST,
+});
+
+const loginError = error => ({
+  type: actionTypes.LOGIN_ERROR,
+  error,
+});
+
+const logoutSuccess = () => ({
   type: actionTypes.LOGOUT_SUCCESS,
+});
+
+const logoutRequest = () => ({
+  type: actionTypes.LOGOUT_REQUEST,
+});
+
+const logoutError = error => ({
+  type: actionTypes.LOGOUT_ERROR,
+  error,
+});
+
+const signUpSuccess = () => ({
+  type: actionTypes.SIGN_UP_SUCCESS,
+});
+
+const signUpRequest = () => ({
+  type: actionTypes.SIGN_UP_REQUEST,
+});
+
+const signUpError = error => ({
+  type: actionTypes.SIGN_UP_ERROR,
+  error,
 });
 
 export const login = user => async dispatch => {
   try {
+    dispatch(loginRequest());
     const { data } = await userService.login({ user });
     await sessionService.saveUser(data.user);
     dispatch(loginSuccess());
   } catch (err) {
+    dispatch(loginError(err));
     throw new SubmissionError({
       _error: err.error,
     });
@@ -30,20 +70,25 @@ export const login = user => async dispatch => {
 
 export const logout = () => async dispatch => {
   try {
+    dispatch(logoutRequest());
     await userService.logout();
     sessionService.deleteSession();
     sessionService.deleteUser();
     dispatch(logoutSuccess());
   } catch (err) {
+    dispatch(logoutError(err));
     throw err;
   }
 };
 
-export const signUp = user => async () => {
+export const signUp = user => async dispatch => {
   try {
+    dispatch(signUpRequest());
     const { data } = await userService.signUp({ user });
     sessionService.saveUser(data.user);
+    dispatch(signUpSuccess());
   } catch (err) {
+    dispatch(signUpError(err));
     throw new SubmissionError({
       _error: err.error,
     });
---
+++ src/hooks/useLoading.js
@@ -0,0 +1,10 @@
+import { useSelector } from 'react-redux';
+import { LOADING } from 'constants/status';
+
+const useLoading = action =>
+  useSelector(({ actionStatus }) => {
+    const { status } = actionStatus[action] || {};
+    return status === LOADING;
+  });
+
+export default useLoading;
