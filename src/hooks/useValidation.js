2779701950396f0b30d71ebb98850f39094c3049
Mauricio Cousillas <mauricio.cousillas@rootstrap.com>
2019-10-23 11:10:29 -0300

    Code improvements

--- src/hooks/useValidation.js
+++ src/hooks/useValidation.js
@@ -3,4 +3,5 @@ import validate from 'validate.js';
 
 validate.validators.presence.options = { allowEmpty: false };
 
-export default constraints => useCallback(values => validate(values, constraints), [constraints]);
+export default (constraints, options = {}) =>
+  useCallback(values => validate(values, constraints, options), [constraints, options]);
