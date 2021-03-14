4a023c89387501c7226ac5de9ba878bfca80440d
BrianJVarley <brianvarley9@gmail.com>
2021-03-14 14:57:09 +0000

    Typescript: Convert project build and test to TypeScript

--- metro.config.js
+++ metro.config.js
@@ -13,5 +13,11 @@ module.exports = {
         inlineRequires: false,
       },
     }),
+    getTransformModulePath() {
+      return require.resolve('react-native-typescript-transformer');
+    },
+    getSourceExts() {
+      return ['ts', 'tsx', 'js', 'jsx'];
+    }
   },
 };
