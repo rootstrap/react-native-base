424edc65a19e788e316df2d10be347c37bf2be46
BrianJVarley <brianvarley9@gmail.com>
2021-04-06 17:07:28 +0100

    Update jest.config to fix test runner

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,21 @@
-module.exports =  {
-    preset: "react-native",
-    transform: {
-      "^.+\\.tsx?$": "ts-jest",
-      "^.+\\.(js|jsx)$": "babel-jest",
-      "node_modules/variables/.+\\.(j|t)sx?$": "babel-jest"
-    },
-    testRegex: "(/src/.*\\.(test|spec))\\.(jsx?|tsx?|ts|js)$",
-    transformIgnorePatterns: [
-      "node_modules/(?!(jest-)?react-native|react-(native|universal|navigation)-(.*)|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|@rootstrap/redux-tools)"
-    ],
-    setupFiles: [
-      "./tests/__mocks__/index.js",
-      "<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js"
-    ],
-    moduleFileExtensions: [
-      "ts",
-      "tsx",
-      "js",
-      "jsx",
-      "json",
-      "node"
-    ]
-  }
+module.exports = {
+  preset: 'react-native',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)?$',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-(native|universal|navigation)-(.*)|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|@rootstrap/redux-tools)',
+  ],
+  setupFiles: [
+    './tests/__mocks__/index.js',
+    '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+};
