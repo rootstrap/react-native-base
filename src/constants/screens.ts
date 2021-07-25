691f451825795b437e75fd2257f60472e39d8291
Brian Varley <brianvarley9@gmail.com>
2021-07-25 18:18:19 +0100

    stock service: move toke param to interceptor (re-use)

--- src/constants/screens.ts
+++ src/constants/screens.ts
@@ -4,7 +4,7 @@ export const AUTH_STACK = 'AuthStack';
 
 // PARENT SCREENS
 export const LOGIN_SCREEN = 'LoginScreen';
-export const MAIN_SCREEN = 'MainScreen';
+export const MAIN_SCREEN = 'Stocks Feed';
 export const SIGN_UP_SCREEN = 'SignUpScreen';
 
 // CHILD SCREENS
