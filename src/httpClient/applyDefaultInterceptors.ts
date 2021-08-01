451156b60a2dc8bff3c054a2b786f5e372ef8f7f
Brian Varley <brianvarley9@gmail.com>
2021-08-01 15:43:03 +0100

    Add list overlay to display tile labels

--- src/httpClient/applyDefaultInterceptors.ts
+++ src/httpClient/applyDefaultInterceptors.ts
@@ -10,53 +10,56 @@ const CLIENT = 'client';
 const UNAUTHORIZED = 401;
 
 export default (
-  store: Store,
-  client: {
-    interceptors: {
-      request: { use: (arg0: (config: any) => any) => void };
-      response: {
-        use: (arg0: (response: any) => Promise<any>, arg1: (error: any) => Promise<never>) => void;
-      };
-    };
-  },
-) => {
-  client.interceptors.request.use((config: { headers: any; data: any; params: any }) => {
-    const { info } = store.getState().session;
-    const { data, headers } = config;
-    if (info) {
-      const { client, uid } = info;
-      config.headers = {
-        ...headers,
-        client,
-        uid,
-      };
-    }
-    config.params = {[ACCESS_TOKEN]: Config.IEX_TOKEN}
-
-    config.data = humps.decamelizeKeys(data);
-    return config;
-  });
-
-  client.interceptors.response.use(
-    async (response: { data: any; headers?: any }) => {
-      const { headers, data } = response;
-      const token = headers[ACCESS_TOKEN];
-      if (token) {
-        const session = {
-          uid: headers[UID],
-          client: headers[CLIENT],
+    store: Store,
+    client: {
+        interceptors: {
+            request: { use: (arg0: (config: any) => any) => void };
+            response: {
+                use: (
+                    arg0: (response: any) => Promise<any>,
+                    arg1: (error: any) => Promise<never>,
+                ) => void;
+            };
         };
-        store.dispatch(updateSession(session));
-      }
-      response.data = humps.camelizeKeys(data);
-      return response;
     },
-    (error: { response: { status: number } }) => {
-      if (error.response && error.response.status === UNAUTHORIZED) {
-        store.dispatch(logout());
-      }
+) => {
+    client.interceptors.request.use((config: { headers: any; data: any; params: any }) => {
+        const { info } = store.getState().session;
+        const { data, headers } = config;
+        if (info) {
+            const { client, uid } = info;
+            config.headers = {
+                ...headers,
+                client,
+                uid,
+            };
+        }
+        config.params = { [ACCESS_TOKEN]: Config.IEX_TOKEN };
 
-      return Promise.reject(error);
-    },
-  );
+        config.data = humps.decamelizeKeys(data);
+        return config;
+    });
+
+    client.interceptors.response.use(
+        async (response: { data: any; headers?: any }) => {
+            const { headers, data } = response;
+            const token = headers[ACCESS_TOKEN];
+            if (token) {
+                const session = {
+                    uid: headers[UID],
+                    client: headers[CLIENT],
+                };
+                store.dispatch(updateSession(session));
+            }
+            response.data = humps.camelizeKeys(data);
+            return response;
+        },
+        (error: { response: { status: number } }) => {
+            if (error.response && error.response.status === UNAUTHORIZED) {
+                store.dispatch(logout());
+            }
+
+            return Promise.reject(error);
+        },
+    );
 };
