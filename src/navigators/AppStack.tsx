6f76e25cfcc8ecad62f35d16258c547b5af20860
Brian Varley <brianvarley9@gmail.com>
2022-01-08 20:31:03 +0100

    Add icon to stack header

--- src/navigators/AppStack.tsx
+++ src/navigators/AppStack.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MAIN_SCREEN } from '../constants/screens';
 import MainScreen from '../screens/MainScreen';
+import { Image } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const AppStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen name={MAIN_SCREEN} component={MainScreen} />
-  </Stack.Navigator>
+    <Stack.Navigator>
+        <Stack.Screen
+            name={MAIN_SCREEN}
+            component={MainScreen}
+            options={{
+                title: MAIN_SCREEN,
+                headerTitle: () => (
+                    <Image
+                        style={{ width: 40, height: 40 }}
+                        source={require('../assets/easy.stocks-icon.png')}
+                    />
+                ),
+                headerStyle: {
+                    backgroundColor: '#47ac7d',
+                },
+            }}
+        />
+    </Stack.Navigator>
 );
 
 export default AppStack;
