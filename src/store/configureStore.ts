2aa0fc1e53ead5979ab34dfe55b94ff74ab179f8
Brian Varley <brianvarley9@gmail.com>
2022-11-12 21:31:04 +0100

    Replace community package with -> @react-native-async-storage/async-storage

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { thunkMiddleware } from '@rootstrap/redux-tools';
 import { createLogger } from 'redux-logger';
