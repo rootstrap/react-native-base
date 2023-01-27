476dbfe225039c2ef7dcaff432ef5741bd6e7616
Brian Varley <brianvarley9@gmail.com>
2023-01-27 16:28:56 +0100

    Add script to gen icons, update image assets

---
+++ .iconsetrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    imagePath: './easy.stocks-icon.png',
+    android: true,
+    // adaptiveIconBackground: './assets/app-icon/background.png',
+    // adaptiveIconForeground: './assets/app-icon/foreground.png',
+};
---
+++ android/app/src/debug/res/README.md
@@ -0,0 +1,2 @@
+Resizing images: Run sips -z 500 1024 --out easy-stocks-promo.png playstore-icon.png
+
