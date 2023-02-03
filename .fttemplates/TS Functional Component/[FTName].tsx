e5a718cb3aa7c0eae9ff9cbe83b7df2d9524abcc
Marcos Iriarte Bosnic <marcosmatias_iriarte@hotmail.com>
2023-02-03 14:32:28 -0300

    Merge changes and commit suggestions

--- .fttemplates/TS Functional Component/[FTName].tsx
+++ .fttemplates/TS Functional Component/[FTName].tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import styles from './[FTName].styles';
+interface <FTName>Props {}
 
-const <FTName>: React.FunctionComponent = () => (
+const <FTName>= ({}: <FTName>Props) => (
   <View>
     <Text> REPLACE ME</Text>
   </View>
