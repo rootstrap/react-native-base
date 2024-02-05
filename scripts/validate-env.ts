93ead8dffd297220b286ee1878603dc40552fe29
Fernanda Toledo <fertoledo93@gmail.com>
2024-02-05 11:49:34 -0300

    .env file for development should be .env.dev

--- scripts/validate-env.ts
+++ scripts/validate-env.ts
@@ -4,17 +4,15 @@ import path from 'path';
 import Environment from '../src/@types/env';
 
 const APP_ENV = process.env.APP_ENV ?? 'dev';
-const isDev = APP_ENV === 'dev';
-const envFileName = isDev ? '.env' : `.env.${APP_ENV}`;
-const envFile = path.join(__dirname, `../${envFileName}`);
+const envFile = path.join(__dirname, `../.env.${APP_ENV}`);
 
 (() => {
   const fileExists = fs.existsSync(envFile);
   if (!fileExists) {
     console.error(
-      `❌ Missing ${envFileName} file. Make sure you have ${envFileName} file in the root directory.`,
+      `❌ Missing .env.${APP_ENV} file. Make sure you have .env.${APP_ENV} file in the root directory.`,
     );
-    throw new Error(`Missing ${envFileName} file. Check terminal for more details`);
+    throw new Error(`Missing .env.${APP_ENV} file. Check terminal for more details`);
   }
   const content = fs.readFileSync(envFile, 'utf8');
   const envVars = content.split('\n');
@@ -35,7 +33,7 @@ const envFile = path.join(__dirname, `../${envFileName}`);
     console.error(
       '❌ Invalid environment variables:',
       parsed.error.flatten().fieldErrors,
-      `\n❌ Missing variables in ${envFileName} file, Make sure all required variables are defined in the ${envFileName} file.`,
+      `\n❌ Missing variables in .env.${APP_ENV} file, Make sure all required variables are defined in the .env.${APP_ENV} file.`,
     );
     throw new Error('Invalid environment variables, Check terminal for more details ');
   }
