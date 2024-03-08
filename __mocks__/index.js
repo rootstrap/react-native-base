4c760ddd3286aa9ac315c43b9af96acf95a6ce6d
Fernanda Toledo <fertoledo93@gmail.com>
2024-03-08 11:50:48 -0300

    Fix tests

--- __mocks__/index.js
+++ __mocks__/index.js
@@ -1,5 +1,22 @@
 jest.mock('react-native-bootsplash', () => ({
   hide: jest.fn().mockResolvedValueOnce(),
+  useHideAnimation: jest.fn(({ animate }) => {
+    // Simulate animation immediately
+    animate();
+    return {
+      container: {},
+      logo: {},
+      brand: {},
+    };
+  }),
 }));
+
+jest.mock('common/AnimatedSplash', () =>
+  jest.fn(({ onAnimationEnd }) => {
+    // Call onAnimationEnd immediately to simulate the splash screen disappearing
+    onAnimationEnd();
+    return null;
+  }),
+);
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
