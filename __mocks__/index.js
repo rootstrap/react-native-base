e9dc23bbd45e894135d4d60b1c2113e3b8bf6c16
Juan Simón <juansimon18.js@gmail.com>
2024-05-03 16:33:07 -0500

    Update index.js

--- __mocks__/index.js
+++ __mocks__/index.js
@@ -1,5 +1,3 @@
-import { dir } from 'console';
-import { init } from 'i18next';
 import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
 
 jest.mock('react-native-device-info', () => mockRNDeviceInfo);
