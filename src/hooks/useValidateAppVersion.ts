9c99f1ec4c0ca56636920de0ea708d078595aea4
Junior Sancho <junior.sancho@rootstrap.com>
2024-05-10 11:26:28 -0500

    EsLint line error disabled

--- src/hooks/useValidateAppVersion.ts
+++ src/hooks/useValidateAppVersion.ts
@@ -56,6 +56,7 @@ export const useValidateAppVersion = () => {
 
   useEffect(() => {
     checkUpdate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { isUpdateAvailable, openStore };
